eff6e1442b87ffbf0d36773d6746f05cc8995747
samiondevicellm <sami.dhif@gmail.com>
2026-07-03 14:29:58 +0100

    Update build.gradle.kts

--- prototype-android/app/build.gradle.kts
+++ prototype-android/app/build.gradle.kts
@@ -47,4 +47,5 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.activity:activity-ktx:1.8.0")
 }
